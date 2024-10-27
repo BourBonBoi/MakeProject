14e31a9dafe8248d9e0fedff324c72636909545e
bourbonboi <artemmine9292@gmail.com>
2024-10-27 11:47:29 +0900

    add header

--- src/app.js
+++ src/app.js
@@ -1,18 +1,16 @@
 const express = require('express');
 const path = require('path');
-const indexRouter = require('./routes/index');
-
+const indexRouter = require('./routes/index'); 
 const app = express();
+const PORT = 3000;
 
-// Подключение статических файлов
-app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.static('public'));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
-// Подключение маршрутов
-app.use('/', indexRouter);
-
+app.use('/', indexRouter); 
 
 // Запуск сервера
-const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Сервер запущен на http://localhost:${PORT}`);
 });
