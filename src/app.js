6c37ada9032ce6a67319c66969e0702fb04881ac
bourbonboi <artemmine9292@gmail.com>
2024-11-01 16:41:43 +0900

    add db and register logic

--- src/app.js
+++ src/app.js
@@ -1,12 +1,26 @@
 const express = require('express');
 const path = require('path');
 
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
 const indexRouter = require('./routes/index');
 const registerRouter = require('./routes/register');
 const loginRouter = require('./routes/login');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+// Подключение к MongoDB
+mongoose.connect('mongodb://localhost:27017/MakeProject', {
+
+})
+.then(() => {
+    console.log('Успешно подключено к MongoDB');
+})
+.catch((error) => {
+    console.error('Ошибка подключения к MongoDB:', error);
+});
 
 // Поддержка статических файлов
 app.use(express.static('public'));
--- src/controllers/registerController.js
+++ src/controllers/registerController.js
@@ -1,14 +1,32 @@
+const User = require('../models/Users')
+
 const registerController = {
     showRegisterPage: (req, res) => {
         res.render('register'); // Отображение страницы регистрации
     },
 
-    handleRegister: (req, res) => {
+    // Обработка данных регистрации (POST-запрос)
+    handleRegister: async (req, res) => {
         const { username, email, password } = req.body;
-        // Здесь должна быть логика по обработке регистрации (например, сохранение пользователя в БД)
-        console.log('Регистрация:', { username, email, password });
-        res.redirect('/'); // Перенаправление на главную страницу после регистрации
+        if (!username || !email || !password) {
+            return res.status(400).send('Все поля обязательны для заполнения.');
+        }
+
+        const newUser = new User({
+            username,
+            email,
+            password,
+        });
+
+        try {
+            await newUser.save();
+            res.status(201).send('Пользователь успешно зарегистрирован!');
+        } catch (error) {
+            if (error.code === 11000) {
+                return res.status(400).send('Пользователь с таким email уже существует.');
+            }
+            res.status(500).send('Ошибка при регистрации пользователя: ' + error.message);
+        }
     },
 };
-
 module.exports = registerController;
